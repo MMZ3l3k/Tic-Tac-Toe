05e600e0e436d09abc437346e45da84c92b14bb8
MMZ3l3k <mateusz.matczuk.it@gmail.com>
2023-10-07 12:39:41 +0200

    Multiplayer Tic-Tac-Toe

---
+++ src/main/java/org/example/TicTacToe.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        String [][] grid = new String[3][3];
+
+        grid [0][0] = " ";
+        grid [0][1] = " ";
+        grid [0][2] = " ";
+        grid [1][0] = " ";
+        grid [1][1] = " ";
+        grid [1][2] = " ";
+        grid [2][0] = " ";
+        grid [2][1] = " ";
+        grid [2][2] = " ";
+
+        System.out.println("The value at spot 0,0 is " + grid [0][0]);
+
+
+    }
+}
